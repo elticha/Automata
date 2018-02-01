49f75aa852c5b4fb98a2dba387dbc67d16533913
Luis Fernando Hernández Morales <163189@ids.upchiapas.edu.mx>
2018-02-01 17:15:09 -0600

    Propuesta
    ...

--- Switch/src/pkgswitch/Switch.java
+++ Switch/src/pkgswitch/Switch.java
@@ -1,9 +1,6 @@
 package pkgswitch;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Switch {
 
@@ -12,92 +9,64 @@ public class Switch {
 
     public static void main(String[] args) {
         boolean ok = true;
+        boolean nextPart = false;
         Scanner input = new Scanner(System.in);
         String cadena;
         String elementos[];  //División de la cadena por %            
         //Cadena original = Switch(#VAR#){%case#VAR#:print("TEXT");break;%default:print("TEXT");}
+        //Cadena original = 
+        // Switch(VAR)#{case VAR:print("TEXT");break;default:print("TEXT");}
         System.out.print("Ingrese la cadena >> ");
         cadena = input.nextLine();
-        elementos = cadena.split("%");
-        String elementos_gato1[] = null;// = elementos[0].split("#");
-        String elementos_gato2[] = null;// = elementos[1].split("#");
-        String elementos_gato3[] = null;// = elementos[2].split("\"");
-
-        try {
-            elementos_gato1 = elementos[0].split("#");
-            elementos_gato2 = elementos[1].split("#");
-            elementos_gato3 = elementos[2].split("\"");
-        } catch (IndexOutOfBoundsException e) {
-            printErrorMessage();
-            ok = false;
+        String separacionDeSwitch[] = cadena.split("#"); // -> Esto genera lo siguiente:
+                                                         // [0] Switch(var)
+                                                         // [1] {caseVAR:print("TEXT");break;default:print("TEXT");}
+        String separacionSemiColon[] = separacionDeSwitch[1].split(";");
+        
+        for(int i = 0; i < separacionSemiColon.length; i++){
+            System.out.println(separacionSemiColon[i]);
         }
-
-        if (ok) {
-            for (int i = 0; i < elementos.length; i++) {
-                System.out.println(elementos[i]);
-            }
-            System.out.println("**************************");
-            for (int i = 0; i < elementos_gato1.length; i++) {
-                System.out.println(elementos_gato1[i]);
-            }
-            System.out.println("****************");
-            for (int i = 0; i < elementos_gato2.length; i++) {
-                System.out.println(elementos_gato2[i]);
-            }
-            System.out.println(elementos[2]);
-            System.out.println("**************************");
-
-            String ult[] = elementos_gato2[2].split("\"");
-            for (int i = 0; i < ult.length; i++) {
-                System.out.println(ult[i]);
-            }
-            System.out.println("****************");
-            for (int i = 0; i < elementos_gato3.length; i++) {
-                System.out.println(elementos_gato3[i]);
-            }
-            System.out.println("************************\n\n\n");
-            
-            if (q0(elementos_gato1)) {
-                if (q1(elementos_gato1[1])) {//Si recibo puros numeros
-                    if (q2(elementos_gato1[1])) {
-                        //q4
-                        if(q4(elementos_gato1[2])){
-                            System.out.println(" >>>>>> " + elementos_gato2[0]);
-                            if(q5(elementos_gato2[0])){
-                                
-                            }else{
-                                printErrorMessage();
-                            }
-                        }else{
-                            printErrorMessage();
-                        }
-                    } else {
+        //Verificar que el usuario haya introducido -> Switch(----)
+        if(q0(separacionDeSwitch[0])){
+            //Validar la variable del usuario
+            String variableUsuario = separacionDeSwitch[0].substring(7,separacionDeSwitch[0].length()-1);
+            System.out.println("Variable = " + variableUsuario);    //OK
+            //Validar si va a q2 o a q3
+            if(q1(variableUsuario)){//Si es una variable de números
+                //validar con q2 que la variable sea un número
+                if(q2(variableUsuario)){
+                    //proceder al estado q4
+                    if(q4(separacionDeSwitch[0].substring(separacionDeSwitch[0].length()-1, separacionDeSwitch[0].length()))){
+                        //Si la primera parte termina con ")" ----> OK
+                        nextPart = true;
+                    }else{
                         printErrorMessage();
                     }
-                } else if (!q1(elementos_gato1[1])) {
-                    //c va a q3()
-                    //q3(elementos_gato1[1]);
-                    if (q3(elementos_gato1[1])) {
-                        //q4
-                        if(q4(elementos_gato1[2])){
-                            System.out.println(" >>>>>> " + elementos_gato2[0]);
-                            if(q5(elementos_gato2[0])){
-                                
-                            }else{
-                                printErrorMessage();
-                            }
-                        }else{
-                            printErrorMessage();
-                        }
-                    } else {
-                        printErrorMessage();
-                    }
-                } else {
+                }else{
                     printErrorMessage();
                 }
-            } else {
-                printErrorMessage();
+            }else{// si es una variable de caracteres
+                //validar con q3 que la variable sea un string
+                if(q3(variableUsuario)){
+                    //proceder al estado q4
+                    if(q4(separacionDeSwitch[0].substring(separacionDeSwitch[0].length()-1, separacionDeSwitch[0].length()))){
+                        //Si la primera parte termina con ")" ----> OK
+                        nextPart = true;
+                    }else{
+                        printErrorMessage();
+                    }
+                }else{
+                    printErrorMessage();
+                }
             }
+        }else{
+            printErrorMessage();
+        }
+        
+        
+        //  Si la parte del Switch salió con éxito, continuar con el complemento
+        if(nextPart){
+            
         }
     }
 
@@ -105,16 +74,19 @@ public class Switch {
         System.err.println("¡¡¡ CADENA NO ADMITIDA !!!");
     }
 
-    static boolean q0(String s[]) {
-        boolean ok = true;
-        if (!s[0].equals("Switch(")) {
-            ok = false;
-        }
-        return ok;
+    static boolean q0(String s) {
+       boolean ok = false;
+       String subcadena = s.substring(0,7); //---> "Switch(" --> OK
+       if(subcadena.equals("Switch(") && s.charAt(s.length()-1) == ')'){ //---> "Switch(" ")"
+        ok = true;
+       }
+       return ok;
     }
 
+    //Si es letra retorna FALSE
+    //Si es número retorna TRUE
     static boolean q1(String cadenita) {
-        if (isLetter(cadenita.charAt(0))) {
+        if(isLetter(cadenita.charAt(0))) {
             return false;
         } else {
             return true;
@@ -152,7 +124,7 @@ public class Switch {
     }
     
     static boolean q4(String s){
-        if(s.equals(")}")){
+        if(s.equals(")")){
             return true;
         }else{
             return false;
